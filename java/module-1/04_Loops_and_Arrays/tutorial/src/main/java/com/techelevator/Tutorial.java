fc460e7e5a3e1c401361b8150cba67e74b27175d
Spencer Cindia <Spencer3033@gmail.com>
2022-03-10 11:31:22 -0500

    working on lecture

--- java/module-1/04_Loops_and_Arrays/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ java/module-1/04_Loops_and_Arrays/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -3,13 +3,22 @@ package com.techelevator;
 public class Tutorial {
 
     public static void main(String[] args) {
-
-        // write your code here
-
-
-
-
-
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+        }
+        int[] forecastTemperatures = new int [5];
+        forecastTemperatures[0] = 72;
+        forecastTemperatures[1] = 78;
+        forecastTemperatures[2] = 58;
+        forecastTemperatures[3] = 79;
+        forecastTemperatures[4] = 74;
+        forecastTemperatures[2] += 10;
+        for (int i = 0; i <= forecastTemperatures.length; i++) {
+            System.out.println(forecastTemperatures[i]);
+        }
 
     }
 }
