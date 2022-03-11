bcdc0b53ad184629088b30d86b188525439d29ab
Spencer Cindia <Spencer3033@gmail.com>
2022-03-11 16:03:09 -0500

    turn in homework and see if decimals cause a fail

--- java/module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
+++ java/module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,31 @@
 package com.techelevator;
+import java.util.Scanner;
+
+//m = f * 0.3048
+//f = m * 3.2808399
+
+
 
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		final double METER_CONVERSION = 3.2808399;
+		final double FEET_CONVERSION = 0.3048;
 
+		System.out.println("Let's convert meters and feet! ");
+		System.out.println("Enter your numerical measurement here: ");
+		String userMeasurement = scanner.nextLine();
+		System.out.println("Now enter 'm' for meters or 'f' for feet: ");
+		String userMeasurementType = scanner.nextLine();
+		double finalConversion = 0;
+		if (userMeasurementType.equals("m")) {
+			finalConversion = Integer.parseInt(userMeasurement)*METER_CONVERSION;
+			System.out.println(userMeasurement+userMeasurementType+" is "+finalConversion+"f");
+		} else {
+			finalConversion = Integer.parseInt(userMeasurement)*FEET_CONVERSION;
+			System.out.println(userMeasurement+userMeasurementType+" is "+finalConversion+"m");
+		}
 	}
 
 }
--- java/module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
+++ java/module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,35 @@
 package com.techelevator;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("I will convert temp from Celsius to Fahrenheit or vice versa! ");
+		System.out.println("What is the numerical value of the temp you want to convert? ");
+		String tempInput = scanner.nextLine();
+		System.out.println("And what is the type of temp? (C or F) ");
+		String tempType = scanner.nextLine();
 
+		double finalConversion = 0;
+		String letterF = "F";
+		String letterC = "C";
+		boolean isCelsius = false;
+
+		if (tempType.equals("C")) {
+			finalConversion = ((Integer.parseInt(tempInput)-32)/1.8);
+			isCelsius = true;
+		} else {
+			finalConversion = ((Integer.parseInt(tempInput)*1.8)+32);
+		}
+		if (isCelsius) {
+			System.out.println(finalConversion + letterC + " is " + tempInput + letterF);
+		} else {
+			System.out.println(finalConversion + letterF + " is " + tempInput + letterC);
+		}
 	}
 
 }
