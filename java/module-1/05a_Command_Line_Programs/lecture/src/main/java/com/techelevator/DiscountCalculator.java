e074abb3c93f7c88d87ef31bce45135fabbbbac3
Spencer Cindia <Spencer3033@gmail.com>
2022-03-11 12:07:31 -0500

    lecture

--- java/module-1/05a_Command_Line_Programs/lecture/src/main/java/com/techelevator/DiscountCalculator.java
+++ java/module-1/05a_Command_Line_Programs/lecture/src/main/java/com/techelevator/DiscountCalculator.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 class DiscountCalculator {
 
-    /**
-     * The main method is the start and end of our program
-     */
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Discount Calculator");
 
-        // Prompt the user for a discount amount
-        // The answer needs to be saved as a double
         System.out.print("Enter the discount amount (w/out percentage): ");
-
-
+        String lineInput = scanner.nextLine();
+        double discountPercent = Double.parseDouble(lineInput);
 
         // Prompt the user for a series of prices
         System.out.print("Please provide a series of prices (space separated): ");
 
+        lineInput = scanner.nextLine();
 
-
+        String[] priceArray = lineInput.split(" ");
+        double[] priceArrayDou = new double[priceArray.length];
+        for (int i = 0; i < priceArray.length; i++) {
+            System.out.println(Double.parseDouble(priceArray[i])*(discountPercent/100));
+        }
 
 
 
