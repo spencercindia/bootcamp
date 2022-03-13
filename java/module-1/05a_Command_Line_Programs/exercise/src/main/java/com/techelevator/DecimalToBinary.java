2995ad1a061c3d13f42c7bdcd346cc869abcb5a6
Spencer Cindia <Spencer3033@gmail.com>
2022-03-13 13:06:33 -0400

    final homework push

--- java/module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ java/module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,9 +1,21 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("I am here to convert decimal to binary! ");
+		System.out.println("Please enter your decimal value here: ");
+		String userDecimal = scanner.nextLine();
+
+		String[] decimalArrayString = userDecimal.split(" ");
+
+		for (int i = 0; i < decimalArrayString.length; i++) {
+			int currentIndex = Integer.parseInt(decimalArrayString[i]);
+			System.out.println(Integer.toBinaryString(currentIndex));
+		}
 	}
-
 }
--- java/module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ java/module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,36 @@
 package com.techelevator;
 
+import java.util.Scanner;
+import java.util.Vector;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
 
-	}
 
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("I will give you the Fibonacci sequence leading up to the value you give me!");
+		System.out.println("Please enter your value here: ");
+		String userInput = scanner.nextLine();
+		int userInputInt = Integer.parseInt(userInput);
+
+		int fibStarterOne = 0;
+		int fibStarterTwo = 1;
+		int fibNew;
+
+		System.out.print(fibStarterOne + ", " + fibStarterTwo);
+		while (fibStarterOne < userInputInt)
+		{
+			fibNew = fibStarterOne + fibStarterTwo;
+			if (fibNew > userInputInt)
+			{
+				break;
+			}
+			System.out.print(", " + fibNew);
+			fibStarterOne = fibStarterTwo;
+			fibStarterTwo = fibNew;
+
+		}
+	}
 }
