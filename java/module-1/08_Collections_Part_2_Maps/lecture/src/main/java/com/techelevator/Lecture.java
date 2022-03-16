d40ad565bcf6e6b4b7cb2637953b4bf8d7460486
Spencer Cindia <Spencer3033@gmail.com>
2022-03-16 11:47:44 -0400

    i dont even know why

--- java/module-1/08_Collections_Part_2_Maps/lecture/src/main/java/com/techelevator/Lecture.java
+++ java/module-1/08_Collections_Part_2_Maps/lecture/src/main/java/com/techelevator/Lecture.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.sql.Struct;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -12,6 +13,27 @@ public class Lecture {
 		System.out.println("        MAPS");
 		System.out.println("####################");
 		System.out.println();
+
+		Map<Integer, String> myMap = new HashMap<>();
+
+		myMap.put(1, "more time");
+		myMap.put(2, "much");
+		myMap.put(3, "musketeers");
+		myMap.put(4, "square");
+		myMap.put(5, "guys");
+		myMap.put(6, "flags");
+		myMap.put(7, "Brad Pitt");
+
+		Set<Integer> keys = myMap.keySet();
+		String searchTerm = "Brad";
+		for (Integer k : keys){
+			if (myMap.get(k).contains(searchTerm)){
+				System.out.println(true);
+			}
+		}
+
+		
+
 
 
 	}
---
+++ java/module-1/08_Collections_Part_2_Maps/lecture/src/main/java/com/techelevator/SetPractice.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+import java.util.Set;
+import java.util.HashSet;
+
+public class SetPractice {
+    public static void main(String[] args) {
+        Set<String> cartoonShows = new HashSet<>();
+        cartoonShows.add("Ed Edd & Eddy");
+        cartoonShows.add("Rick & Morty");
+        cartoonShows.add("Simpsons");
+        cartoonShows.add("Courage The Cowardly Dog");
+
+
+    }
+
+}
