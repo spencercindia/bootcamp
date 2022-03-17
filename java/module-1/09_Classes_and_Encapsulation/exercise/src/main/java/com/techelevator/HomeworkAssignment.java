9833037046dab12d69717950a5badc7f87d6e6ce
Spencer Cindia <Spencer3033@gmail.com>
2022-03-17 18:10:13 -0400

    i dont understand anything

--- java/module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ java/module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,50 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+    public int earnedMarks;
+    public int possibleMarks;
+    public String submitterName;
+    public String letterGrade;
 
+    public HomeworkAssignment(int possibleMarks, String submitterName){
+        this.submitterName = submitterName;
+        this.possibleMarks = possibleMarks;
+    }
+
+    public void findLetterGrade(){
+        double gradePercentage = earnedMarks*100 / possibleMarks;
+        if (gradePercentage >= 90){
+            this.letterGrade = "A";
+        } else if (gradePercentage >= 80){
+            this.letterGrade = "B";
+        } else if (gradePercentage >= 70){
+            this.letterGrade = "C";
+        } else if (gradePercentage >= 60){
+            this.letterGrade = "D";
+        } else if (gradePercentage < 59){
+            this.letterGrade = "F";
+        } else {
+            this.letterGrade = "";
+        }
+    }
+
+    public void setEarnedMarks(int earnedMarks) {
+        this.earnedMarks = earnedMarks;
+    }
+
+    public int getEarnedMarks() {
+        return earnedMarks;
+    }
+
+    public int getPossibleMarks() {
+        return possibleMarks;
+    }
+
+    public String getSubmitterName() {
+        return submitterName;
+    }
+
+    public String getLetterGrade() {
+        return letterGrade;
+    }
 }
