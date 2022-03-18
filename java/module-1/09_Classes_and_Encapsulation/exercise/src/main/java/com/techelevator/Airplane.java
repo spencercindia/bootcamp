76b274c245ccdf1fdf47bb30a5b090bda43558e7
Spencer Cindia <Spencer3033@gmail.com>
2022-03-18 08:56:28 -0400

    airplanetest

--- java/module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
+++ java/module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
@@ -1,5 +1,56 @@
 package com.techelevator;
 
 public class Airplane {
+    private String planeNumber;
+    private int totalFirstClassSeats;
+    private int bookedFirstClassSeats;
+    private int totalCoachSeats;
+    private int bookedCoachSeats;
 
+    public Airplane(String planeNumber, int totalFirstClassSeats, int totalCoachSeats){
+        this.planeNumber = planeNumber;
+        this.totalFirstClassSeats = totalFirstClassSeats;
+        this.totalCoachSeats = totalCoachSeats;
+    }
+
+    public boolean reserveSeats(boolean forFirstClass, int totalNumberOfSeats){
+        if (forFirstClass){
+            if (totalFirstClassSeats-bookedFirstClassSeats >= totalNumberOfSeats){
+                bookedFirstClassSeats+=totalNumberOfSeats;
+                return true;
+            } else return false;
+        } else if (!forFirstClass){
+            if (totalCoachSeats-bookedCoachSeats >= totalNumberOfSeats){
+                bookedCoachSeats+=totalCoachSeats;
+                return true;
+            } else return false;
+        } else return false;
+    }
+
+    public int getAvailableFirstClassSeats(){
+        return totalFirstClassSeats - bookedFirstClassSeats;
+    }
+    public int getAvailableCoachSeats(){
+        return totalCoachSeats - bookedCoachSeats;
+    }
+
+    public String getPlaneNumber() {
+        return planeNumber;
+    }
+
+    public int getTotalFirstClassSeats() {
+        return totalFirstClassSeats;
+    }
+
+    public int getBookedFirstClassSeats() {
+        return bookedFirstClassSeats;
+    }
+
+    public int getTotalCoachSeats() {
+        return totalCoachSeats;
+    }
+
+    public int getBookedCoachSeats() {
+        return bookedCoachSeats;
+    }
 }
