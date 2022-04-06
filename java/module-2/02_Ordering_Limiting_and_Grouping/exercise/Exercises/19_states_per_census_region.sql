c25bd5acd07c91ac230b4ccec59da11efd7390d4
Spencer Cindia <Spencer3033@gmail.com>
2022-04-05 20:09:02 -0400

    that's all I got

--- java/module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/19_states_per_census_region.sql
+++ java/module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/19_states_per_census_region.sql
@@ -2,4 +2,7 @@
 -- Order the results from highest to lowest.
 -- (Note: DC is in a census region, but the territories aren't, so the sum of the counts will equal 51).
 -- (4 rows)
-
+SELECT census_region, COUNT(state_name) AS num_states
+FROM state
+WHERE census_region IS NOT NULL
+ORDER BY COUNT(state_name);
--- java/module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
+++ java/module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
@@ -1,4 +1,7 @@
 -- 20. The state abbreviation, and population of the largest city (name column 'city_population') of all states, territories, and districts.
 -- Order the results from highest to lowest populations.
 -- (56 rows)
-
+SELECT population, state_abbreviation AS city_population
+FROM city
+GROUP BY state_abbreviation
+ORDER BY population DESC;
