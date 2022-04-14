d5e11252c8cb2c29d9ce8fa5deb3b2dd91ef3028
Spencer Cindia <Spencer3033@gmail.com>
2022-04-14 09:38:48 -0400

    homework done

--- java/module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcEmployeeDao.java
+++ java/module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcEmployeeDao.java
@@ -27,35 +27,71 @@ public class JdbcEmployeeDao implements EmployeeDao {
 		String sql = "select * from employee;";
 		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
 		while (results.next()) {
-			employees.add(mapRowToProject(results));
+			employees.add(mapRowToEmployee(results));
 		}
 		return employees;
 	}
 
 	@Override
 	public List<Employee> searchEmployeesByName(String firstNameSearch, String lastNameSearch) {
-		return List.of(new Employee());
+		List<Employee> employeesByName = new ArrayList<>();
+		String sql = "select * from employee where (first_name ilike ? and last_name ilike ?);";
+		SqlRowSet results;
+		if (firstNameSearch.length() == 0) {
+			if (lastNameSearch.length() == 0) {
+				sql = "select * from employee";
+				results = jdbcTemplate.queryForRowSet(sql);
+			} else {
+				sql = "select * from employee where last_name ilike ?;";
+				results = jdbcTemplate.queryForRowSet(sql, "%" + lastNameSearch + "%");
+			}
+		} else {
+			results = jdbcTemplate.queryForRowSet(sql, "%" + firstNameSearch + "%", "%" + lastNameSearch + "%");
+		}
+		while (results.next()) {
+			employeesByName.add(mapRowToEmployee(results));
+		}
+		return employeesByName;
 	}
 
 	@Override
 	public List<Employee> getEmployeesByProjectId(Long projectId) {
-		return new ArrayList<>();
+		List<Employee> employeesById = new ArrayList<>();
+		String sql = "select * from employee join project_employee using(employee_id) where project_id = ?;";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, projectId);
+		while (results.next()) {
+			employeesById.add(mapRowToEmployee(results));
+		}
+		return employeesById;
 	}
 
 	@Override
 	public void addEmployeeToProject(Long projectId, Long employeeId) {
+		String sql = "insert into project_employee (project_id, employee_id) values (?, ?);";
+		if (projectId != null && employeeId != null) {
+			jdbcTemplate.update(sql, projectId, employeeId);
+		}
+
 	}
 
 	@Override
 	public void removeEmployeeFromProject(Long projectId, Long employeeId) {
+		String sql = "delete from project_employee where employee_id = ?;";
+		jdbcTemplate.update(sql, employeeId);
 	}
 
 	@Override
 	public List<Employee> getEmployeesWithoutProjects() {
-		return new ArrayList<>();
+		List<Employee> employeeList = new ArrayList<>();
+		String sql = "select * from employee left join project_employee using(employee_id) where project_id is null";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		while (results.next()) {
+			employeeList.add(mapRowToEmployee(results));
+		}
+		return employeeList;
 	}
 
-	private Employee mapRowToProject(SqlRowSet results) {
+	private Employee mapRowToEmployee(SqlRowSet results) {
 		Employee employee = new Employee();
 		employee.setId(results.getLong("employee_id"));
 		employee.setDepartmentId(results.getLong("department_id"));
