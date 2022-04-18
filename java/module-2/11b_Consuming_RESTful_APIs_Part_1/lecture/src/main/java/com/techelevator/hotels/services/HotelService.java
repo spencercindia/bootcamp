09558972d2d3d7b4da2636b9ce8906dd490fc219
Spencer Cindia <Spencer3033@gmail.com>
2022-04-18 17:24:44 -0400

    monday homework

--- java/module-2/11b_Consuming_RESTful_APIs_Part_1/lecture/src/main/java/com/techelevator/hotels/services/HotelService.java
+++ java/module-2/11b_Consuming_RESTful_APIs_Part_1/lecture/src/main/java/com/techelevator/hotels/services/HotelService.java
@@ -11,27 +11,27 @@ public class HotelService {
     private final RestTemplate restTemplate = new RestTemplate();
 
     public Hotel[] listHotels() {
-        return null;
+        return restTemplate.getForObject(API_BASE_URL + "hotels", Hotel[].class);
     }
 
     public Review[] listReviews() {
-        return null;
+        return restTemplate.getForObject(API_BASE_URL + "reviews", Review[].class);
     }
 
     public Hotel getHotelById(int id) {
-        return null;
+        return restTemplate.getForObject(API_BASE_URL + "hotels/" + id, Hotel.class);
     }
 
     public Review[] getReviewsByHotelId(int hotelID) {
-        return null;
+        return restTemplate.getForObject(API_BASE_URL + "hotels/" + hotelID + "/reviews", Review[].class);
     }
 
     public Hotel[] getHotelsByStarRating(int stars) {
-        return null;
+        return restTemplate.getForObject(API_BASE_URL + "hotels?stars=" + stars, Hotel[].class);
     }
 
     public City getWithCustomQuery(){
-        return null;
+        return restTemplate.getForObject("https://api.teleport.org/api/cities/geonameid%3A971421/", City.class);
     }
 
 }
