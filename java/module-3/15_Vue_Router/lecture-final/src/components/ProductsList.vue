8370c3526f49af1b91fc098eaaa3dc81f0956cac
joshuamack <joshua.mack@techelevator.com>
2022-05-24 11:55:36 -0400

    Lecture Final for Vue Router

---
+++ java/module-3/15_Vue_Router/lecture-final/src/components/ProductsList.vue
@@ -0,0 +1,43 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th># Reviews</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="product in $store.state.products" v-bind:key="product.id">
+        <td>{{ product.id }}</td>
+        <!-- This should take us to localhost:8080/products/:id-->
+        <td>
+        <router-link v-bind:to="{name: 'product-detail', params: {id: product.id}}">
+          {{ product.name }}
+        </router-link>
+        </td>
+        <td>{{ product.reviews.length }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "products-list"
+};
+</script>
+
+<style style="scoped">
+th,
+td {
+  text-align: left;
+}
+td {
+  padding-right: 10px;
+  vertical-align: top;
+}
+tr:nth-child(even) {
+  background-color: rgb(238, 238, 238);
+}
+</style>
