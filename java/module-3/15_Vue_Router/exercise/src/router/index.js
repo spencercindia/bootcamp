d64124a3afb5f9d4cf284c609d39b94290c38a71
Spencer Cindia <Spencer3033@gmail.com>
2022-05-24 19:50:13 -0400

    push before pull

--- java/module-3/15_Vue_Router/exercise/src/router/index.js
+++ java/module-3/15_Vue_Router/exercise/src/router/index.js
@@ -4,7 +4,11 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }
 ];
 
 const router = new VueRouter({
---
+++ java/module-3/15_Vue_Router/exercise/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import BestSellerList from "../components/BestSellerList.vue";
+import NewReleasesList from "../components/NewReleasesList.vue";
+
+export default {
+  name: "Home",
+  components: { NewReleasesList, BestSellerList },
+};
+</script>
+<style scoped>
+</style>
